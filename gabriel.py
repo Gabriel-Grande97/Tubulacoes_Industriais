016af292a1084183133d53b8d57cd8a3294538da
Gabriel-Grande97 <grandealmeida@gmail.com>
2020-10-20 20:04:15 -0300

    codigo diametro

--- gabriel.py
+++ gabriel.py
@@ -1,7 +1,12 @@
-'''
-Gabriel
+''' Python 3.8
+Universidade de Brasília
+Código para Dimensionamento de diâmetr   Autores: Gabriel Grande e Pedro Paulo
+Disciplina: Tubulações Industriais       Professor: Thiago Doca
+Data: 20/10/2020
 '''
 
-gabriel = 5
+velocidade_estimada = 2
 
-print(gabriel)
+diametro_tubulacao_principal = 80
+
+diametro_tubulacao_secundaria = 40
